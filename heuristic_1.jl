0124ae8cab6eb46169d7af7f8524175cea2563d4
Pablo González <jp_glzf@hotmail.com>
2023-01-04 15:42:51 -0600

    Division of prize and square root of distance in the correct order

--- heuristic_1.jl
+++ heuristic_1.jl
@@ -15,6 +15,7 @@ recollected_prize = 0
 while (cities.city ∉ visited) || (travel_cost < restriction_cost)
     current_city = cities[cities[!, :city] .== last(I), :]
 
-    cities[!,:prize_cost_ratio] = cities[!,:prize] ./ ((first(current_city[!,:x_axis],1)) .- cities[!,:x_axis]) .^ 2 + ((first(current_city[!,:y_axis],1)) .- cities[!,:y_axis]) .^ 2
+    cities[!,:prize_cost_ratio] = ((first(current_city[!,:x_axis],1)) .- cities[!,:x_axis]) .^ 2 + ((first(current_city[!,:y_axis],1)) .- cities[!,:y_axis]) .^ 2
     select(cities, :prize_cost_ratio, :prize_cost_ratio => ByRow(sqrt))
+    cities[!,:prize_cost_ratio] = cities[!,:prize] ./ cities[!,:prize_cost_ratio]
 end
