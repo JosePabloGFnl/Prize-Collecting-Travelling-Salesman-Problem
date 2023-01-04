827c6dfa4bc7993849e20e08e97352a96fdca3de
Pablo González <jp_glzf@hotmail.com>
2023-01-04 15:37:57 -0600

    Prize-Cost ratio of cities succesfully calculated

--- heuristic_1.jl
+++ heuristic_1.jl
@@ -12,15 +12,9 @@ recollected_prize = 0
 
 #while loop that ends when all cities are visited or the total travel cost reaches its limit
 
-#while (cities.city ∉ visited) || (travel_cost < restriction_cost)
-#    current_city = cities[cities[!, :city] .== last(I), :]
-#    for city in cities
-#        prize_travel_cost_ratio = city.prize/(sqrt((current_city.x - city.x) ^ 2 + (current_city.y - city.y) ^ 2))
-#    end
-#end
+while (cities.city ∉ visited) || (travel_cost < restriction_cost)
+    current_city = cities[cities[!, :city] .== last(I), :]
 
-current_city = cities[cities[!, :city] .== last(I), :]
-
-cities[!,:prize_cost_ratio] = cities.prize/(sqrt((first(current_city.x_axis,1) - cities.x_axis) ^ 2 + ((first(current_city.y_axis,1) - cities.y_axis) ^ 2)))
-
-print(cities)
+    cities[!,:prize_cost_ratio] = cities[!,:prize] ./ ((first(current_city[!,:x_axis],1)) .- cities[!,:x_axis]) .^ 2 + ((first(current_city[!,:y_axis],1)) .- cities[!,:y_axis]) .^ 2
+    select(cities, :prize_cost_ratio, :prize_cost_ratio => ByRow(sqrt))
+end
