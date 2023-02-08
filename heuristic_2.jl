05360c49dc4401c14876ad1ac8092915141e98ae
Pablo González <jp_glzf@hotmail.com>
2023-02-08 13:50:23 -0600

    Initialize with a small tour
    Might improve steps to calculate shortest city from 1

---
+++ heuristic_2.jl
@@ -0,0 +1,36 @@
+using CSV
+#Cheapest Insertion-type Heuristic
+using DataFrames
+
+#variable initialization
+cities = DataFrame(CSV.File("generated_cities.csv"))
+
+@time begin
+minimum_profit = 450
+
+#function for minimum distance
+min_from_1 = copy(cities)
+min_from_1[!,:distances] = ((first(min_from_1[1,:x_axis],1)) .- min_from_1[!,:x_axis]) .^ 2 + ((first(min_from_1[1,:y_axis],1)) .- min_from_1[!,:y_axis]) .^ 2
+delete!(min_from_1, [1])
+select(min_from_1, :distances, :distances => ByRow(sqrt))
+min_city = findmin(min_from_1[!, :distances])[2]
+
+I = [Int64(cities[1, :city])]
+
+push!(I, Int64(min_from_1[min_city, :city]), 1)
+
+able_to_visited = cities[.!(cities[:, :city] .∈ I), :city]
+recollected_prize = Int64(cities[1, :prize])
+total_travel_cost = 0
+
+#while loop that ends when all cities are visited or the total travel cost reaches its limit
+
+while (length(able_to_visited) ≠ 0) && (recollected_prize < minimum_profit)
+    
+
+end
+
+println(I)
+println(recollected_prize)
+println(total_travel_cost)
+end
