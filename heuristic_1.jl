65411f74263aef8eec9c672ccdfe948843d93946
Pablo González <jp_glzf@hotmail.com>
2023-02-16 18:10:02 -0600

    Improvements and changed to function in heuristic_1

--- heuristic_1.jl
+++ heuristic_1.jl
@@ -1,41 +1,38 @@
-using CSV
-#Nearest Neighbor-type Heuristic
-using DataFrames
+using CSV, DataFrames
 
-#variable initialization
-cities = DataFrame(CSV.File("generated_cities.csv"))
+function nearest_neighbor_heuristic(cities_file::AbstractString, minimum_profit::Int64)
+    # load cities data
+    cities = DataFrame(CSV.File(cities_file))
 
-@time begin
-minimum_profit = 450
-I = [Int64(cities[1, :city])]
-able_to_visited = cities[.!(cities[:, :city] .∈ I), :city]
-recollected_prize = Int64(cities[1, :prize])
-total_travel_cost = 0
+    # variable initialization
+    I = [cities[1, :city]]
+    able_to_visited = cities[.!(cities[:, :city] .∈ I), :city]
+    recollected_prize = cities[1, :prize]
+    total_travel_cost = 0
 
-#while loop that ends when all cities are visited or the total travel cost reaches its limit
+    # while loop that ends when all cities are visited or the total travel cost reaches its limit
+    while (!isempty(able_to_visited)) && (recollected_prize < minimum_profit)
+        # selects the current city to be the point of search based on the most recently inserted one in the tour
+        current_city = cities[cities[!, :city] .== last(I), :]
 
-while (length(able_to_visited) ≠ 0) && (recollected_prize < minimum_profit)
-    #selects the current city to be the point of search based on the most recently inserted one in the tour
-    current_city = cities[cities[!, :city] .== last(I), :]
+        # checks the distances by coordinates between the selected city and the available
+        cities[!,:distances] = ((first(current_city[!,:x_axis],1)) .- cities[!,:x_axis]) .^ 2 + ((first(current_city[!,:y_axis],1)) .- cities[!,:y_axis]) .^ 2
+        select(cities, :distances, :distances => ByRow(sqrt))
+        cities[!,:prize_cost_ratio] = cities[!,:prize] ./ cities[!,:distances]
 
-    #checks the distances by coordinates between the selected city and the available
-    cities[!,:distances] = ((first(current_city[!,:x_axis],1)) .- cities[!,:x_axis]) .^ 2 + ((first(current_city[!,:y_axis],1)) .- cities[!,:y_axis]) .^ 2
-    select(cities, :distances, :distances => ByRow(sqrt))
-    cities[!,:prize_cost_ratio] = cities[!,:prize] ./ cities[!,:distances]
-    
-    #add the one with the biggest prize_cost_ratio
-    added_city = cities[findall(in(able_to_visited), cities[!,:city]), :]
-    added_city = added_city[sortperm(added_city[:, :prize_cost_ratio], rev=true), :]
+        # add the one with the biggest prize_cost_ratio
+        added_city = cities[findall(in(able_to_visited), cities[!,:city]), :]
+        added_city = added_city[sortperm(added_city[:, :prize_cost_ratio], rev=true), :]
 
-    global recollected_prize += added_city[!,:prize][1]
-    global total_travel_cost += added_city[!,:distances][1]
-    
-    append!( I, added_city[1, :city] )
-    deleteat!( able_to_visited, findfirst(able_to_visited .== added_city[1, :city]) )
+        recollected_prize += added_city[1, :prize]
 
+    end
+
+    return I, recollected_prize, total_travel_cost
 end
 
+# Example usage:
+I, recollected_prize, total_travel_cost = nearest_neighbor_heuristic("generated_cities.csv", 450)
 println(I)
 println(recollected_prize)
 println(total_travel_cost)
-end
--- heuristic_2.jl
+++ heuristic_2.jl
@@ -6,41 +6,45 @@ using DataFrames
 cities = DataFrame(CSV.File("generated_cities.csv"))
 
 @time begin
-minimum_profit = 450
+    minimum_profit = 450
 
-#function for minimum distance
-min_from_1 = deepcopy(cities)
-min_from_1[!,:distances] = ((first(min_from_1[1,:x_axis],1)) .- min_from_1[!,:x_axis]) .^ 2 + ((first(min_from_1[1,:y_axis],1)) .- min_from_1[!,:y_axis]) .^ 2
-delete!(min_from_1, [1])
-select(min_from_1, :distances, :distances => ByRow(sqrt))
-min_city = findmin(min_from_1[!, :distances])[2]
+    #function for minimum distance
+    min_from_1 = deepcopy(cities)
+    min_from_1[!,:distances] = ((first(min_from_1[1,:x_axis],1)) .- min_from_1[!,:x_axis]) .^ 2 + ((first(min_from_1[1,:y_axis],1)) .- min_from_1[!,:y_axis]) .^ 2
+    delete!(min_from_1, [1])
+    select(min_from_1, :distances, :distances => ByRow(sqrt))
+    min_city = findmin(min_from_1[!, :distances])[2]
 
-total_travel_cost = min_city
+    total_travel_cost = min_city
 
-I = [Int64(cities[1, :city])]
+    I = [Int64(cities[1, :city])]
 
-push!(I, Int64(min_from_1[min_city, :city]), 1)
+    push!(I, Int64(min_from_1[min_city, :city]), 1)
 
-able_to_visited = setdiff(cities[:, :city], I)
+    able_to_visited = setdiff(cities[:, :city], I)
 
-recollected_prize = sum(cities.prize[cities.city .∈ Ref(I)])
+    recollected_prize = sum(cities.prize[cities.city .∈ Ref(I)])
 
-#while loop that ends when all cities are visited or the total travel cost reaches its limit
+    #while loop that ends when all cities are visited or the total travel cost reaches its limit
 
-while (length(able_to_visited) ≠ 0) && (recollected_prize < minimum_profit)
+    while (length(able_to_visited) ≠ 0) && (recollected_prize < minimum_profit)
 
-    #checks the distances by coordinates between the selected city and the available
-    cities[!,:prize_cost_ratio] = cities[!,:prize] ./ total_travel_cost
-    
-    #add the one with the biggest prize_cost_ratio
-    added_city = cities[findall(in(able_to_visited), cities[!,:city]), :]
-    added_city = added_city[sortperm(added_city[:, :prize_cost_ratio], rev=true), :]
+        #checks the distances by coordinates between the selected city and the available
+        cities[!,:prize_cost_ratio] = cities[!,:prize] ./ total_travel_cost
+        
+        #add the one with the biggest prize_cost_ratio
+        added_city = cities[findall(in(able_to_visited), cities[!,:city]), :]
+        added_city = added_city[sortperm(added_city[:, :prize_cost_ratio], rev=true), :]
 
-    global recollected_prize += added_city[!,:prize][1]
+        recollected_prize += added_city[1, :prize]
+        global total_travel_cost += added_city[!,:distances][1]
+        
+        append!( I, added_city[1, :city] )
+        deleteat!( able_to_visited, findfirst(able_to_visited .== added_city[1, :city]) )
 
-end
+    end
 
-println(I)
-println(recollected_prize)
-println(total_travel_cost)
+    println(I)
+    println(recollected_prize)
+    println(total_travel_cost)
 end
