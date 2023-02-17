3b895c61546cc7a7713afdf942949d68f9da945e
Pablo González <jp_glzf@hotmail.com>
2023-02-17 17:05:29 -0600

    Insertion phase completed
    Might look for improvements if necessary

--- heuristic_2.jl
+++ heuristic_2.jl
@@ -22,24 +22,36 @@ function cheapest_insertion_heuristic(cities_file::AbstractString, minimum_profi
 
     recollected_prize = sum(cities.prize[cities.city .∈ Ref(I)])
 
-    #while loop that ends when all cities are visited or the total travel cost reaches its limit
+    #while loop that ends when all cities are visited and the recollected prize in the tour is greater than the minimum profit
 
     while (length(able_to_visited) ≠ 0) && (recollected_prize < minimum_profit)
 
         #checks the distances by coordinates between the selected city and the available
         cities[!,:prize_cost_ratio] = cities[!,:prize] ./ total_travel_cost
         
-        #add the one with the biggest prize_cost_ratio
+        #Selection phase
         added_city = cities[findall(in(able_to_visited), cities[!,:city]), :]
         added_city = added_city[sortperm(added_city[:, :prize_cost_ratio], rev=true), :]
 
         recollected_prize += added_city[1, :prize]
 
+        #Insertion phase
+        distances = [sqrt((cities[added_city[1, :city], :x_axis] - cities[I[i], :x_axis])^2 + (cities[added_city[1, :city], :y_axis] - cities[I[i], :y_axis])^2) + 
+                    sqrt((cities[added_city[1, :city], :x_axis] - cities[I[i+1], :x_axis])^2 + (cities[added_city[1, :city], :y_axis] - cities[I[i+1], :y_axis])^2) -
+                    sqrt((cities[I[i+1], :x_axis] - cities[I[i], :x_axis])^2 + (cities[I[i+1], :y_axis] - cities[I[i], :y_axis])^2)
+                    for i in 1:length(I)-1]
+
+        min_index = argmin(distances)
+        insert!(I, min_index+1, Int64(added_city[1, :city]))
+
+
+        able_to_visited = setdiff(cities[:, :city], I)
+
     end
     return I, recollected_prize, total_travel_cost
 end
 
-I, recollected_prize, total_travel_cost = nearest_neighbor_heuristic("generated_cities.csv", 450)
+I, recollected_prize, total_travel_cost = cheapest_insertion_heuristic("generated_cities.csv", 450)
 println(I)
 println(recollected_prize)
 println(total_travel_cost)
