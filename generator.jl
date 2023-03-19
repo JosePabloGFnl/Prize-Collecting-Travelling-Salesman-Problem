0437a7273465c6c004edb11e1f11817adb9e0e3b
Pablo González <jp_glzf@hotmail.com>
2023-03-18 20:22:44 -0600

    max_axis and max_prize changed to values from literature
    Dellamico

--- generator.jl
+++ generator.jl
@@ -2,11 +2,15 @@ using DataFrames,DotEnv, DelimitedFiles
 DotEnv.load()
 
 cities = parse(Int64, ENV["QUANTITY_CITIES"])
+min_axis = parse(Int64, ENV["QUANTITY_CITIES"])
+min_prize = parse(Int64, ENV["QUANTITY_CITIES"])
+max_axis = parse(Int64, ENV["QUANTITY_CITIES"])
+max_prize = parse(Int64, ENV["QUANTITY_CITIES"])
 
 df = DataFrame(city = 1:cities, 
-               x_axis = rand(0:(cities*2),cities),
-               y_axis = rand(0:(cities*2),cities),
-               prize = rand(0:Int(round(cities/2)),cities)
+               x_axis = rand(min_axis:max_axis,cities),
+               y_axis = rand(min_axis:max_axis,cities),
+               prize = rand(min_prize:max_prize,cities)
                )
 
 writedlm(ENV["GENERATED_FILE"], Matrix(df))
