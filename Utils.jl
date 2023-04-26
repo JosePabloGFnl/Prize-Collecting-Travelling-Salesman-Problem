6616f0e717a3b76312faa06990734052639f0207
Pablo González <jp_glzf@hotmail.com>
2023-04-26 13:49:00 -0600

    Alpha variable added for minimum profit calculation
    Improves obtaining the minimum profit.

--- Utils.jl
+++ Utils.jl
@@ -1,12 +1,15 @@
 module Utils
+using DotEnv
+DotEnv.load()
 
 export calculate_minimum_profit
 
 function calculate_minimum_profit(cities::Matrix)
     n = size(cities, 1)
+    alpha=parse(Float64, ENV["ALPHA"])
     min_prize = minimum(cities[:, 4])
     max_prize = maximum(cities[:, 4])
-    return Int(round((n)*(min_prize+max_prize)/2))
+    return Int(round((n*alpha)*(min_prize+max_prize)/2))
 end
 
 end # module
