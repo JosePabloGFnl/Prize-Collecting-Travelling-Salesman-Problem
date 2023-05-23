d063185eadd60efe0be4a820019ef6f6692e80a9
Pablo González <jp_glzf@hotmail.com>
2023-05-23 17:42:31 -0600

    While loop created
    As well as variables Improve, last and city_to_remove which represents a random city in the tour that will be swapped by another one.

--- local_search.jl
+++ local_search.jl
@@ -15,9 +15,18 @@ function node_swap(cities_file::AbstractString)
     # Create a DataFrame from the remaining rows
     cities = cities[setdiff(1:end, [1,2]), :]
     minimum_profit = calculate_minimum_profit(cities)
+
     # calculate distances between all pairs of cities
     n = size(cities, 1)
-    dist_mat = sqrt.(sum((reshape(cities[:, 2:3], 1, n, 2) .- reshape(cities[:, 2:3], n, 1, 2)).^2, dims=3))    
+    dist_mat = sqrt.(sum((reshape(cities[:, 2:3], 1, n, 2) .- reshape(cities[:, 2:3], n, 1, 2)).^2, dims=3)) 
+    
+    Improve = true
+
+    while (Improve = true)
+        last = I
+        city_to_remove = rand(I)
+    end
+
 end
 
 cities, I = node_swap(ENV["GENERATED_FILE"])
