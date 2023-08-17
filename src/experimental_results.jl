ccbe02ac6d10a0b0bd61a20326cb9e5e7a4424e8
Pablo Gonzalez Flores <jp_glzf@hotmail.com>
2023-08-16 18:54:44 -0600

    Improvement indicator automatized

--- src/experimental_results.jl
+++ src/experimental_results.jl
@@ -10,14 +10,16 @@ results = DataFrame(
     "H1 Recollected Prize" => Int[],
     "H1 Prize/Cost Ratio" => Float64[],
     "H1 Local Search Total Travel Cost" => Float64[],
+    "Improved? H1" => String[],
     "H2 Total Travel Cost" => Float64[],
     "H2 Recollected Prize" => Int[],
     "H2 Prize/Cost Ratio" => Float64[],
     "H2 Local Search Total Travel Cost" => Float64[],
+    "Improved? H2" => String[],
 )
 
 function experiments_table(iteration, total_travel_cost_h1::Float64, recollected_prize_h1::Int, improved_travel_cost_h1::Float64, total_travel_cost_h2::Float64, recollected_prize_h2::Int, improved_travel_cost_h2::Float64)
-    push!(results, (iteration, total_travel_cost_h1, recollected_prize_h1, recollected_prize_h1 / total_travel_cost_h1, improved_travel_cost_h1, total_travel_cost_h2, recollected_prize_h2, recollected_prize_h2 / total_travel_cost_h2, improved_travel_cost_h2))
+    push!(results, (iteration, total_travel_cost_h1, recollected_prize_h1, recollected_prize_h1 / total_travel_cost_h1, improved_travel_cost_h1, total_travel_cost_h1 > improved_travel_cost_h1 ? "Yes" : "No", total_travel_cost_h2, recollected_prize_h2, recollected_prize_h2 / total_travel_cost_h2, improved_travel_cost_h2, total_travel_cost_h2 > improved_travel_cost_h2 ? "Yes" : "No"))
     return results
 end
 
