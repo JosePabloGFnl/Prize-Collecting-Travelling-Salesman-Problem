5bee88e6476c85f65d26cf34cc47b1edc7cb9319
Pablo Gonzalez Flores <jp_glzf@hotmail.com>
2023-08-23 19:46:32 -0600

    Plot saved to PNG instead of SVG

--- src/graph_results.jl
+++ src/graph_results.jl
@@ -1,16 +1,13 @@
 module graph_results
-using DotEnv, Gadfly, DataFrames, DelimitedFiles
-
-DotEnv.load()
+using Gadfly, DataFrames, Cairo, Fontconfig
 
 function graph_cities(cities::Matrix, I::Array)
     df_cities = DataFrame(x_axis = cities[:, 2], y_axis = cities[:, 3], label = string.(cities[:, 1]))
     
     p = plot(df_cities, x=:x_axis, y=:y_axis, Geom.point)
     
-    img = SVG(ENV["PLOT"], 14cm, 8cm)
+    img = PNG(ENV["PLOT"], 50cm, 25cm)
     draw(img, p)
-
 end
 
 end
