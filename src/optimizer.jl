b8bebde126826c56eabd2cb11a2ac010786239cb
Pablo González <jp_glzf@hotmail.com>
2023-11-15 19:45:18 -0600

    Adapted for PCTSP model
    Not for use at the moment due to variables needing instance data

--- src/optimizer.jl
+++ src/optimizer.jl
@@ -1,23 +1,34 @@
 using JuMP, Gurobi
 
-#Create Gurobi model
+# Define the number of nodes (n)
+n = 5  # You can change this based on your problem
+
+# Create a Gurobi model
 model = Model(Gurobi.Optimizer)
 
-#Define Decision Variables
-@variable(model, x >= 0)
-@variable(model, y >= 0)
+# Decision variables
+@variable(model, x[1:n, 1:n], Bin)  # Binary variables x[i, j]
+@variable(model, y[1:n], Bin)        # Binary variables y[i]
 
-#Set Objective Function
-@objective(model, Max, 2x + 3y)
+# Objective function
+# Assuming cost_matrix is a matrix of travel costs
+@objective(model, Min, sum(cost_matrix[i, j] * x[i, j] for i in 1:n, j in 1:n))
 
-#Add Constraints
-@constraint(model, x + y <= 10)
-@constraint(model, 2x - y <= 5)
+# Constraints using array comprehensions
+@constraint(model, [i in 1:n], sum(x[j, i] for j in setdiff(1:n, [i])) - y[i] == 0)
+@constraint(model, [j in 1:n], sum(x[i, j] for i in setdiff(1:n, [j])) - y[j] == 0)
 
-#Optimize the Model
+# Additional constraint
+# Replace this with your actual constraint based on the third equation in your LaTeX model
+# For example, if you want to ensure the total weight of selected nodes is greater than or equal to w0
+@constraint(model, sum(w[i] * y[i] for i in 1:n) >= w0)
+
+# Solve the model
 optimize!(model)
 
-#Retrieve and Print the Solution
-println("Optimal x = ", value(x))
-println("Optimal y = ", value(y))
-println("Optimal objective value = ", objective_value(model))
+# Print the solution
+println("Objective value: ", objective_value(model))
+println("Optimal x:")
+println(value.(x))
+println("Optimal y:")
+println(value.(y))
