85aeb934ee98305f34493197b153e7d7aca68efd
Pablo González <jp_glzf@hotmail.com>
2023-11-15 19:30:15 -0600

    Model template with decision variables, objective function and constraints

--- src/optimizer.jl
+++ src/optimizer.jl
@@ -2,5 +2,22 @@ using JuMP, Gurobi
 
 #Create Gurobi model
 model = Model(Gurobi.Optimizer)
-set_optimizer_attribute(model, "TimeLimit", 100)
-set_optimizer_attribute(model, "Presolve", 0)
+
+#Define Decision Variables
+@variable(model, x >= 0)
+@variable(model, y >= 0)
+
+#Set Objective Function
+@objective(model, Max, 2x + 3y)
+
+#Add Constraints
+@constraint(model, x + y <= 10)
+@constraint(model, 2x - y <= 5)
+
+#Optimize the Model
+optimize!(model)
+
+#Retrieve and Print the Solution
+println("Optimal x = ", value(x))
+println("Optimal y = ", value(y))
+println("Optimal objective value = ", objective_value(model))
