51db83c5beea8d71477250dbc8fdc52b4a38fbc6
Pablo González <jp_glzf@hotmail.com>
2024-04-09 22:41:42 -0600

    Distance matrix added

--- src/main.jl
+++ src/main.jl
@@ -9,9 +9,9 @@ DotEnv.load()
 iterations = parse(Int64, ENV["ITERATIONS"])
 
 for i in 1:iterations
-    filename = generator.instance_generator(i)
-    recollected_prize_h1, total_travel_cost_h1, improved_travel_cost_h1, optimal_value_h1, optimality_gap_h1 = heuristic_1.nearest_neighbor_heuristic(filename)
-    recollected_prize_h2, total_travel_cost_h2, improved_travel_cost_h2, optimal_value_h2, optimality_gap_h2 = heuristic_2.cheapest_insertion_heuristic(filename)
+    filename, distances = generator.instance_generator(i)
+    recollected_prize_h1, total_travel_cost_h1, improved_travel_cost_h1, optimal_value_h1, optimality_gap_h1 = heuristic_1.nearest_neighbor_heuristic(filename, distances)
+    recollected_prize_h2, total_travel_cost_h2, improved_travel_cost_h2, optimal_value_h2, optimality_gap_h2 = heuristic_2.cheapest_insertion_heuristic(filename, distances)
     global(results) = experimental_results.experiments_table(i, total_travel_cost_h1, recollected_prize_h1, improved_travel_cost_h1, optimal_value_h1, optimality_gap_h1, total_travel_cost_h2, recollected_prize_h2, improved_travel_cost_h2, optimal_value_h2, optimality_gap_h2)
 end
 
