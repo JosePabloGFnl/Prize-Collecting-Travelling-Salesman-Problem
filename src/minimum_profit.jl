46d4922101885fc64a6a7cb87b733d10a86e73f1
Pablo González <jp_glzf@hotmail.com>
2024-04-09 22:44:30 -0600

    Prize index updated
    city[4] => city[2]

--- src/minimum_profit.jl
+++ src/minimum_profit.jl
@@ -7,7 +7,7 @@ export calculate_minimum_profit
 function calculate_minimum_profit(cities::Matrix)
     n = size(cities, 1)
     alpha=parse(Float64, ENV["ALPHA"])
-    mean = Statistics.mean(cities[:, 4])
+    mean = Statistics.mean(cities[:, 2])
     return Int(round((n*alpha)*(mean)))
 end
 
