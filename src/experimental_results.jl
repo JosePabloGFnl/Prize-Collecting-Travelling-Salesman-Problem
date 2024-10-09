233d3e78700dff8029163a014d5d24d0289f2ab8
Pablo González <jp_glzf@hotmail.com>
2024-10-09 11:14:56 -0600

    'gurobi_time_h1' changed to 'gurobi_time'

--- src/experimental_results.jl
+++ src/experimental_results.jl
@@ -10,7 +10,7 @@ results = DataFrame(
     "Optimal Value" => Float64[],
     "Optimality Gap H1" => Float64[],
     "LS H1 Time" => Float64[],
-    "Gurobi H1 Time" => Float64[],
+    "Gurobi Time" => Float64[],
     "Improved? H1" => String[],
     "H2 Total Travel Cost" => Float64[],
     "H2 Local Search Total Travel Cost" => Float64[],
@@ -19,7 +19,7 @@ results = DataFrame(
     "Improved? H2" => String[],
 )
 
-function experiments_table(iteration, total_travel_cost_h1::Int, improved_travel_cost_h1::Int, optimal_value::Float64, h1_ls_time, gurobi_time_h1, total_travel_cost_h2::Int, improved_travel_cost_h2::Int, h2_ls_time)
+function experiments_table(iteration, total_travel_cost_h1::Int, improved_travel_cost_h1::Int, optimal_value::Float64, h1_ls_time, gurobi_time, total_travel_cost_h2::Int, improved_travel_cost_h2::Int, h2_ls_time)
     push!(results, (
         iteration,
         total_travel_cost_h1,
@@ -27,7 +27,7 @@ function experiments_table(iteration, total_travel_cost_h1::Int, improved_travel
         optimal_value,
         ((abs(improved_travel_cost_h1 - optimal_value) / abs(improved_travel_cost_h1)) * 100),
         h1_ls_time,
-        gurobi_time_h1,
+        gurobi_time,
         total_travel_cost_h1 > improved_travel_cost_h1 ? "Yes" : "No",
         total_travel_cost_h2,
         improved_travel_cost_h2,
